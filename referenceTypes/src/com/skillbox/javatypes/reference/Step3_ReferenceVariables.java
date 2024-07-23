2c1473817e3fc79548464b0efe5167547d4f4cfe
iskanderiv <vonavi.ashas@gmail.com>
2024-07-23 03:10:35 +0300

    added micses 05 and 06 to reference types. added step3 and step4 to reference types

--- referenceTypes/src/com/skillbox/javatypes/reference/Step3_ReferenceVariables.java
+++ referenceTypes/src/com/skillbox/javatypes/reference/Step3_ReferenceVariables.java
@@ -2,16 +2,21 @@ package com.skillbox.javatypes.reference;
 
 public class Step3_ReferenceVariables {
     public static void main(String[] args) {
-        // объявление переменной класса Frog
-        Frog frog;
-        // System.out.println("Теперь frog = " + frog);
-        // выполнили какие-либо действия в программе
-        System.out.println("Выполняем какие-то действия в программе");
-        // присвоили переменную созданному объекту
-        frog = new Frog();
-        System.out.println("Теперь frog = " + frog);
+        // Связывание переменной с объектом
+        Frog frog; // Объявляете переменную: Имя_класса имя_переменной;
+        frog = new Frog(); // Cоздаете объект и сразу связываете его с переменной через присваивание
+        System.out.println("frog = " + frog);
 
-        frog = new Frog();
-        System.out.println("Теперь frog = " + frog);
+        // ПРИМЕР отложенной инициализации переменной
+        // Объявляете переменную: Имя_класса имя_переменной;
+        Frog frog2;
+//        System.out.println("Теперь frog2 = " + frog2);
+        System.out.println("Выполняем какие-то действия в программе");
+        frog2 = new Frog(); // когда понадобилось, создаете объект и присваиваете переменной
+        System.out.println("frog2 = " + frog2);
+
+        // ПРИМЕР инициализации переменной сразу при ее объявлении
+        Frog frog3 = new Frog();
+        System.out.println("frog3 = " + frog3);
     }
 }
---
+++ referenceTypes/src/com/skillbox/javatypes/reference/Step4_WorkingWithReferenceVariables.java
@@ -0,0 +1,29 @@
+package com.skillbox.javatypes.reference;
+
+public class Step4_WorkingWithReferenceVariables {
+    public static void main(String[] args) {
+//        System.out.println("Создание frog1 и frog2");
+//        Frog frog1 = new Frog(); // Зеленая лягушка
+//        Frog frog2 = new Frog(); // Красная лягушка
+//        System.out.println("Зеленая frog1: " + frog1);
+//        System.out.println("Красная frog2: " + frog2);
+//        frog1 = frog2; // обе переменные теперь указывают на Красную лягушку
+//        System.out.println("После операции frog1 = frog2;");
+//        System.out.println("Бывшая Зеленая frog1: " + frog1);
+//        System.out.println("Красная frog2: " + frog2);
+
+        Frog frog3 = null;
+        Frog frog4 = new Frog();
+        System.out.println("Создание frog3 и frog4");
+        System.out.println("frog3: " + frog3);
+        System.out.println("frog4: " + frog4);
+        frog4 = null; // обе переменные теперь указывают на null
+        System.out.println("После операции frog4 = null;");
+        System.out.println("frog3: " + frog3);
+        System.out.println("frog4: " + frog4);
+
+        if (frog4 == null) {
+            System.out.println("Пример проверки ссылки на null в условных операторах");
+        }
+    }
+}
