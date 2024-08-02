e3b0d3f6aee3e1067309c1125a1991bd1c00adbe
iskanderiv <vonavi.ashas@gmail.com>
2024-08-02 17:12:39 +0300

    правки примитивов

--- primitiveTypes/src/com/skillbox/javatypes/primitive/type/Int.java
+++ primitiveTypes/src/com/skillbox/javatypes/primitive/type/Int.java
@@ -7,10 +7,10 @@ public class Int {
 
         // переполнение разрядной сетки как оно есть
         int iPlusOne = i + 1;
-        System.out.println("i = " + i);
-        System.out.println("i в двоичном виде = " + Integer.toBinaryString(i) + " = 01...1 (31 единица)");
-        System.out.println("iPlusOne = " + iPlusOne);
-        System.out.println("iPlusOne в двоичном виде = " + Integer.toBinaryString(iPlusOne) + " = 10...0 (31 нулей)");
+        System.out.println("i = " + i); // i в десятичном виде
+        System.out.println("i в двоичном виде = " + Integer.toBinaryString(i) + " = 01...1 (31 единица)"); // i в двоичном виде
+        System.out.println("iPlusOne = " + iPlusOne); // в десятичном виде
+        System.out.println("iPlusOne в двоичном виде = " + Integer.toBinaryString(iPlusOne) + " = 10...0 (31 нулей)"); // iPlusOne в двоичном виде
 
     }
 }
