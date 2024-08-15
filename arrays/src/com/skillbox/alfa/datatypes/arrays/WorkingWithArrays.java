240123f29df29807bea54f73176f9269a4bf6210
iskanderiv <vonavi.ashas@gmail.com>
2024-08-16 02:41:40 +0300

    правки массивы

--- arrays/src/com/skillbox/alfa/datatypes/arrays/WorkingWithArrays.java
+++ arrays/src/com/skillbox/alfa/datatypes/arrays/WorkingWithArrays.java
@@ -2,24 +2,9 @@ package com.skillbox.alfa.datatypes.arrays;
 
 public class WorkingWithArrays {
     public static void main(String[] args) {
-        // входной массив данных
-        int[] inputArray = new int[]{5, 6, 7, 8, 9, 12, 23, 11, 4, 2, 18, 32, 45, 34, 101, 7, 9, 10};
-        int threshold = 3; // пороговое значение
-        boolean notValid = false; // признак нашли или нет значение меньше порога
-        int notValidIndex = -1; // здесь будет храниться индекс элемента со значением ниже порога
-        for (int i = 0; i < inputArray.length; i++) {
-            if (inputArray[i] < threshold) {
-                notValid = true;
-                notValidIndex = i;
-            }
-        }
-        if (notValid) {
-            System.out.println("В массиве inputArray длиной: " + inputArray.length
-                    + " встречается элемент меньше threshold: " + threshold + ", он имеет позицию " + (notValidIndex + 1));
-        } else {
-            System.out.println("В массиве inputArray длиной: " + inputArray.length
-                    + " нет элемента меньше threshold: " + threshold);
-        }
+        // данные о возрастах сотрудников из зарплатной ведомости
+        int[] ages = new int[]{34, 46, 73, 17, 19, 33, 23, 57, 49, 28, 25, 25, 45, 34, 39, 58, 60, 21, 45, 65, 36, 2};
+        // необходимо проверить, что в наборе возрастов нет сотрудников младше 18 лет
     }
 }
 
